08c3757344d72af4e3b65b09849ffa2bb786b143
Ilham Febriyansyah <144018682+ilhamkf13@users.noreply.github.com>
2024-08-26 09:45:19 +0000

    Second Commit

---
+++ Ilhamkf/Proyek1/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) {
+        String Nama = "Ilham Kukuh Febriyansyah";
+        String npm = "2320506043";
+        String alamat = "Grabag, Magelang";
+        String noHP = "087739165643";
+
+        System.out.println("Nama = " + Nama);
+        System.out.println("NPM = " + npm);
+        System.out.println("Alamat = " + alamat);
+        System.out.println("No HP = " + noHP);
+    }
+}
